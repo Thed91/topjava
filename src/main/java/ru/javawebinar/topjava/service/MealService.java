1630797fe5cd168837a9b7aee9676e292ff30ad8
Fedor Mihadyuk <fedor.mihadyuk@succraft.com>
2019-02-28 17:10:23 +0300

    Add Meal bean

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,19 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+
+
 public interface MealService {
+
+    Meal create(Meal meal);
+
+    void delete(int id);
+
+    Meal get(int id);
+
+    void update(Meal meal);
+
+    Collection<Meal> getAll();
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,44 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.Collection;
+
+@Service
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.delete(id);
+    }
+
+    @Override
+    public Meal get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public void update(Meal meal) {
+        repository.save(meal);
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return repository.getAll();
+    }
 }
